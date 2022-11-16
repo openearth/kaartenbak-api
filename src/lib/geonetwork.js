f1c477376d5e9859de3ffa6f50c45478798fedf4
Arash Paknezad <a.paknezad@hotmail.com>
2022-11-16 09:34:06 +0100

    refactor: refactor code

--- src/lib/geonetwork.js
+++ src/lib/geonetwork.js
@@ -21,19 +21,20 @@ export const geonetworkRequest = async ({
 
   const cookie = me.headers.get('set-cookie')
   const token = cookie.split(';')[0].split('=')[1]
+  const basicAuth =
+    'Basic ' +
+    Buffer.from(
+      process.env.GEONETWORK_API_USERNAME +
+        ':' +
+        process.env.GEONETWORK_API_PASSWORD
+    ).toString('base64')
 
   // Use X-XSRF-TOKEN and Cookie in the request
   return fetch(BASE_URL + url, {
     method: method,
     ...(method !== 'GET' && { body }),
     headers: {
-      Authorization:
-        'Basic ' +
-        Buffer.from(
-          process.env.GEONETWORK_API_USERNAME +
-            ':' +
-            process.env.GEONETWORK_API_PASSWORD
-        ).toString('base64'),
+      Authorization: basicAuth,
       'X-XSRF-TOKEN': token,
       Cookie: cookie.toString(),
       accept: 'application/json',

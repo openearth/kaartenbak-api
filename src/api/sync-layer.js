daf1127be27d10cfbfb666b1011c212b805a51f4
Arash Paknezad <a.paknezad@hotmail.com>
2023-02-17 00:43:58 +0100

    live debugging netlify function (attempt 9)

--- src/api/sync-layer.js
+++ src/api/sync-layer.js
@@ -48,6 +48,8 @@ exports.handler = withServerDefaults(async (event, _) => {
 
   const layerData = JSON.parse(event.body)
 
+  console.log(await fetchLayerXML({ id: layerData.entity.id }))
+
   const { menus } = await datocmsRequest({
     query: viewersWithLayersQuery,
   })

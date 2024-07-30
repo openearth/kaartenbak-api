52656a150fed11e3262cac0eaa489e7ab343ccac
sjoerdbeentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2024-07-30 11:34:31 +0200

    Add sync and fetch viewer layer functionalities

--- src/api/sync-viewer-layer-background.js
+++ src/api/sync-viewer-layer-background.js
@@ -1,4 +1,53 @@
+import { Geonetwork } from '../lib/geonetwork'
+import { datocmsRequest } from '../lib/datocms'
+import { addThumbnailsToRecord } from '../lib/add-thumbnails-to-record'
 import { withServerDefaults } from '../lib/with-server-defaults'
+import { buildMenuTree } from '../lib/build-menu-tree'
+import { findGeonetworkInstances } from '../lib/find-geonetwork-instances'
+import { fetchViewerLayerXML } from '../lib/fetch-viewer-layer-xml'
+import { formatMenusRecursive } from '../lib/format-menu'
+import Mailjet from 'node-mailjet'
+
+const mailjet = new Mailjet({
+  apiKey: process.env.MAILJET_API_TOKEN,
+  apiSecret: process.env.MAILJET_API_SECRET,
+})
+
+const viewersWithLayersQuery = /* graphql */ `
+query viewersWithLayers ($first: IntType, $skip: IntType = 0, $locale: SiteLocale = nl) {
+  menus: allMenus(first: $first, skip: $skip, locale: $locale) {
+    id
+    geonetwork {
+      baseUrl
+      username
+      password
+    }
+    errorNotificationContacts {
+      email
+    }
+    children: viewerLayers {
+      id
+    }
+    parent {
+      id
+    }
+  }
+  _allMenusMeta {
+    count
+  }
+}`
+
+const viewerLayerByIdQuery = /* graphql */ `
+query LayerById($id: ItemId) {
+  viewerLayer(filter: {id: {eq: $id}}) {
+    layer {
+    	thumbnails {
+        filename
+        url
+      } 
+    }
+  }
+}`
 
 export const handler = withServerDefaults(async (event, _) => {
   /* Protect this endpoint by using a token */
@@ -8,5 +57,150 @@ export const handler = withServerDefaults(async (event, _) => {
     }
   }
 
-  console.log('Syncing layer', JSON.parse(event.body, null, 2))
+  const layerData = JSON.parse(event.body)
+
+  const layerId = layerData.entity.id
+
+  const { menus } = await datocmsRequest({
+    query: viewersWithLayersQuery,
+  })
+  const formattedMenus = formatMenusRecursive(menus)
+  const menuTree = buildMenuTree(formattedMenus)
+
+  try {
+    await syncViewerLayers(menuTree, layerData.event_type, layerId)
+  }
+  catch (e) {
+    console.log('The following error occured', e.message)
+
+    // for (let email of findEmailContactsForLayerId(menuTree, layerId)) {
+    //   console.log('Sending email to', email)
+
+    //   await mailjet.post('send', { version: 'v3.1' }).request({
+    //     Messages: [
+    //       {
+    //         From: {
+    //           Email: process.env.MAILJET_FROM_EMAIL,
+    //         },
+    //         To: [
+    //           {
+    //             Email: email,
+    //           },
+    //         ],
+    //         Subject: `Fout bij opslaan metadata voor laag ${layerId}`,
+    //         HTMLPart: e.message,
+    //       },
+    //     ],
+    //   })
+    // }
+  }
+
 })
+
+async function syncViewerLayers(menuTree, eventType, viewerLayerId) {
+  const geonetworkInstances = findGeonetworkInstances(menuTree, viewerLayerId)
+
+  const geonetworkInstancesArray = Array.from(geonetworkInstances)
+
+  const xml = await fetchViewerLayerXML({ id: layerId })
+
+  // Can occur when no update needs to be done (because there is no factsheet or inspireMetadata)
+  if (xml === null) {
+    return
+  }
+
+  const requestsPromises = geonetworkInstancesArray.map(
+    async ([_, geonetworkInstance]) => {
+      const { baseUrl, username, password } = geonetworkInstance
+
+      const geonetwork = new Geonetwork(
+        baseUrl + 'geonetwork/srv/api',
+        username,
+        password
+      )
+
+      switch (eventType) {
+        case 'create': {
+          await geonetwork.recordsRequest({
+            url: '?publishToAll=true',
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: xml,
+          })
+
+          break
+        }
+
+        case 'publish': {
+          await geonetwork.recordsRequest({
+            url: '?uuidProcessing=OVERWRITE&publishToAll=true',
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: xml,
+          })
+
+          break
+        }
+      }
+
+      switch (eventType) {
+        case 'create':
+        case 'publish':
+          const {
+            layer: { thumbnails },
+          } = await datocmsRequest({
+            query: viewerLayerByIdQuery,
+            variables: { id: layerId },
+          })
+
+          await addThumbnailsToRecord(thumbnails, layerId, geonetwork)
+      }
+    }
+  )
+
+  const results = await Promise.allSettled(requestsPromises)
+
+  const errors = results.filter(result => result.status === 'rejected')
+
+  if (errors.length) {
+    const errorMessage = `<ul>${errors.map(error => `<li>${error.reason}</li>`).join('')}</ul>`
+    throw new Error(errorMessage)
+  }
+
+}
+
+function findEmailContactsForLayerId(menuTree, layerId) {
+  const contacts = new Set()
+
+  menuTree.forEach((viewer) => {
+
+    const findInMenu = (menu) => {
+      const { children } = menu
+
+      if (children) {
+        children.forEach((child) => {
+
+          if (child.layer.id === layerId) {
+            const { errorNotificationContacts } = viewer
+
+            if (errorNotificationContacts.length) {
+              for (let { email } of errorNotificationContacts) {
+                contacts.add(email)
+              }
+            }
+          }
+
+          findInMenu(child)
+        })
+      }
+    }
+
+    findInMenu(viewer)
+  })
+
+  return contacts
+}
---
+++ src/lib/fetch-viewer-layer-xml.js
@@ -0,0 +1,184 @@
+import { datocmsRequest } from './datocms'
+import https from 'https'
+import { format as formatInspireMetadataXml } from './format-inspire-metadata-xml'
+import { format as formatFactsheetXml } from './format-factsheet-xml'
+import fetch from 'node-fetch'
+import convert from 'xml-js'
+
+const query = /* graphql */ `
+query LayerById($id: ItemId) {
+  viewerLayer(filter: {id: {eq: $id}}) {
+    id
+    useFactsheetAsMetadata
+    inspireMetadata {
+      _updatedAt
+      citationTitle
+      citationDateDate
+      citationDateDatetype
+      electronicmailaddress
+      role
+      organisationname
+      abstract
+      identificationinfoStatus
+      topiccategories {
+        topicCategoryItem
+      }
+      descriptivekeywordsKeywords {
+        title
+      }
+      resourceconstraintsAccessconstraints
+      resourceconstraintsUseconstraints
+      mdSpatialrepresentationtypecode
+      thesaurusname
+      thesaurusdatum
+      thesaurusdatumType
+      resourceconstraintsUseconstraints
+      hierarchylevel
+      lineageStatement
+      metadatastandardname
+      metadatastandardversion
+      links {
+        protocol
+        url
+        name
+        description
+      }
+    }
+    factsheets {
+      _updatedAt
+      id
+      title
+      titelNaamMeetMonitorprogramma
+      urlOriginalFile
+      naamAansturendeOrganisatie
+      datumVoltooiing
+      datumVanDeBron
+      datumtypeVanDeBron
+      samenvatting
+      identificationinfoStatus
+      doelWaarvoorDataWordenVerzameld
+      onderwerp {
+        topicCategoryItem
+      }
+      naamUitvoerendeDienstOrganisatie
+      rolContactpersoon
+      geografischGebied
+      toepassingsschaal
+      gebruiksbeperkingen
+      overigeBeperkingenInGebruik
+      themas {
+        title
+      }
+      temporeleDekking
+      hierarchieniveau
+      volledigheid
+      nauwkeurigheid
+      algemeneBeschrijvingVanHerkomst
+      inwinningsmethode
+      beschrijvingUitgevoerdeBewerkingen
+      meetvariabelen
+      meetmethodiek
+      soortDataset
+      kostenOpJaarbasis
+      soortenoverzicht
+      habitats
+    }
+    links {
+      protocol
+      url
+      name
+      description
+    }
+    pointOfContactOrganisations {
+      organisationName
+      email
+      rol
+    }
+    layer {
+      name
+      url
+      layer
+      indexableWfsProperties
+    }
+  }
+}
+`
+
+function recursivelyFindLayer(layers, name) {
+  const layerList = Array.isArray(layers)
+    ? layers
+    : [layers]
+
+  for (let layer of layerList) {
+    if (layer.Name && layer.Name._text === name) {
+      return layer
+    }
+
+    if (layer.Layer) {
+      const foundLayer = recursivelyFindLayer(layer.Layer, name)
+      if (foundLayer) {
+        return foundLayer
+      }
+    }
+  }
+
+  return null
+}
+
+export async function fetchViewerLayerXML({ id }) {
+  const { viewerLayer: {
+    layer,
+    ...viewerLayer
+  } } = await datocmsRequest({ query, variables: { id } })
+
+  console.log(layer)
+
+  const data = {
+    layer: {
+      ...layer,
+      ...viewerLayer,
+    }
+  }
+
+  const getCapabilitiesUrl = `${data.layer.url}?service=WMS&request=GetCapabilities`
+
+  const httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+  })
+
+  const capabilitiesXml = await fetch(getCapabilitiesUrl, {
+    agent: httpsAgent,
+  }).then((res) => res.text())
+
+  const capabilities = JSON.parse(
+    convert.xml2json(capabilitiesXml, {
+      compact: true,
+    })
+  )
+
+  const layerInfo = recursivelyFindLayer(capabilities.WMS_Capabilities.Capability.Layer, data.layer.layer)
+
+  let formatted = null
+
+  if (data.layer.useFactsheetAsMetadata) {
+    const factsheet = data.layer.factsheets[0]
+
+    if (factsheet) {
+      formatted = formatFactsheetXml({
+        id,
+        layerInfo,
+        layer: data.layer,
+        factsheet,
+      })
+    }
+
+  } else if (data.layer.inspireMetadata) {
+    formatted = formatInspireMetadataXml({
+      id,
+      layerInfo,
+      layer: data.layer,
+    })
+  }
+
+  return formatted
+}

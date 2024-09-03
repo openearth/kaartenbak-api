df5c1a2dc3fe1106e331ab995230bf6a5185f949
sjoerdbeentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2024-09-03 14:53:57 +0200

    set correct environment

--- src/lib/datocms.js
+++ src/lib/datocms.js
@@ -26,7 +26,7 @@ function executeFetch(query, variables = {}, preview = false) {
     headers: {
       'Content-Type': 'application/json',
       Authorization: process.env.DATO_API_TOKEN,
-      'X-Environment': 'production',
+      'X-Environment': 'main',
     },
     body: JSON.stringify({ query, variables }),
   })

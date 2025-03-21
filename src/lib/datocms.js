2f35fdb0194d01d8d7f34081ae67c3d1ab1a4182
sjoerdbeentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2025-03-21 15:00:41 +0100

    Refactor datocms.js so it supports multiple environments.

--- src/lib/datocms.js
+++ src/lib/datocms.js
@@ -1,96 +1,112 @@
-import fetch from 'node-fetch'
-import { buildClient } from '@datocms/cma-client-node'
-import { curry, times, pipe, split, map } from 'ramda'
-import slugify from '@sindresorhus/slugify'
+import fetch from "node-fetch";
+import { buildClient } from "@datocms/cma-client-node";
+import { curry, times, pipe, split, map } from "ramda";
+import slugify from "@sindresorhus/slugify";
 
 const capitaliseFirstLetter = ([first, ...rest]) =>
-  first.toUpperCase() + rest.join('')
+  first.toUpperCase() + rest.join("");
 const lowerCaseFirstLetter = ([first, ...rest]) =>
-  first.toLowerCase() + rest.join('')
+  first.toLowerCase() + rest.join("");
 const camelCase = pipe(
   slugify,
-  split('-'),
+  split("-"),
   map(capitaliseFirstLetter),
   lowerCaseFirstLetter
-)
+);
 
-const defaultFirst = 100
+const defaultFirst = 100;
 
-function executeFetch(query, variables = {}, preview = false, token = process.env.DATO_API_TOKEN) {
+function executeFetch(
+  query,
+  variables = {},
+  preview = false,
+  token = process.env.DATO_API_TOKEN,
+  environment = "main"
+) {
   const endpoint = preview
-    ? 'https://graphql.datocms.com/preview'
-    : 'https://graphql.datocms.com/'
+    ? "https://graphql.datocms.com/preview"
+    : "https://graphql.datocms.com/";
 
   return fetch(endpoint, {
-    method: 'post',
+    method: "post",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
-      'X-Environment': 'main',
+      "X-Environment": environment,
     },
     body: JSON.stringify({ query, variables }),
   })
     .then((response) => response.json())
     .then((response) => {
-      if (response.errors) throw Error(JSON.stringify(response, null, 4))
-      return response
-    })
+      if (response.errors) throw Error(JSON.stringify(response, null, 4));
+      return response;
+    });
 }
 
 function getPaginatedData(query, variables, preview) {
   return async function (response) {
-    const keyRegex = /_all(.+)Meta/
-    let allKey
+    const keyRegex = /_all(.+)Meta/;
+    let allKey;
 
     try {
-      allKey = Object.keys(response.data).find((key) => keyRegex.test(key))
+      allKey = Object.keys(response.data).find((key) => keyRegex.test(key));
     } catch (error) {
-      console.log({ query, variables, responseData: response })
-      console.log(response)
-      console.error(error)
+      console.log({ query, variables, responseData: response });
+      console.log(response);
+      console.error(error);
     }
 
     if (allKey) {
-      const { count } = response.data[allKey]
-      const [, originalKey] = allKey.match(keyRegex).map(camelCase)
-      const itemsInResponse = response.data[originalKey]
-      const remainingAmount = count - itemsInResponse.length
+      const { count } = response.data[allKey];
+      const [, originalKey] = allKey.match(keyRegex).map(camelCase);
+      const itemsInResponse = response.data[originalKey];
+      const remainingAmount = count - itemsInResponse.length;
       const totalRemainingRequests = Math.ceil(
         remainingAmount / itemsInResponse.length
-      )
+      );
 
       const promises = times((iteration) => {
-        const skip = iteration * variables.first + itemsInResponse.length
-        const currentDate = new Date().toString()
-        const args = [query, { ...variables, skip, currentDate }, preview]
-        return executeFetch(...args)
-      }, totalRemainingRequests)
+        const skip = iteration * variables.first + itemsInResponse.length;
+        const currentDate = new Date().toString();
+        const args = [query, { ...variables, skip, currentDate }, preview];
+        return executeFetch(...args);
+      }, totalRemainingRequests);
 
       await Promise.all(promises).then((responses) =>
         responses.forEach((res) => {
           response.data[originalKey] = [
             ...response.data[originalKey],
             ...res.data[originalKey],
-          ]
+          ];
         })
-      )
+      );
 
-      delete response.data[allKey]
+      delete response.data[allKey];
     }
-    return response
-  }
+    return response;
+  };
 }
 
 function returnData(response) {
-  return response.data
+  return response.data;
 }
 
-export const datocmsRequest = curry(({ query, variables, preview, token: tokenOverride }) => {
-  const args = [query, { first: defaultFirst, ...variables }, preview, tokenOverride]
+export const datocmsRequest = curry(
+  ({ query, variables, preview, token: tokenOverride, environment }) => {
+    const args = [
+      query,
+      { first: defaultFirst, ...variables },
+      preview,
+      tokenOverride,
+      environment,
+    ];
 
-  return executeFetch(...args)
-    .then(getPaginatedData(...args))
-    .then(returnData)
-})
+    return executeFetch(...args)
+      .then(getPaginatedData(...args))
+      .then(returnData);
+  }
+);
 
-export const datocmsClient = buildClient({ apiToken: process.env.DATO_API_TOKEN })
+export const datocmsClient = buildClient({
+  apiToken: process.env.DATO_API_TOKEN,
+});
